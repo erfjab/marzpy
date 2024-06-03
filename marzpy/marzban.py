b8d486ea7a72196de59d28e9522d5a30fe7f5666
Erfan <143827987+erfjab@users.noreply.github.com>
2024-06-04 02:53:21 +0330

    Delete marzpy directory

--- marzpy/marzban.py
+++
@@ -1,9 +0,0 @@
-from .api import Methods
-
-
-class Marzban(Methods):
-    def __init__(self, username: str, password: str, panel_address: str) -> None:
-        super().__init__(username, password, panel_address)
-        self.username = username
-        self.password = password
-        self.panel_address = panel_address
